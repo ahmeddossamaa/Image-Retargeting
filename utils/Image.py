19977369da8ddaa4ca1fcac6d27bb3db2995a051
Ahmed Osama <70448761+ahmeddossamaa@users.noreply.github.com>
2024-02-02 08:07:59 +0200

    Image class update

--- utils/Image.py
+++ utils/Image.py
@@ -2,19 +2,17 @@ from cv2 import imread, IMREAD_GRAYSCALE, IMREAD_UNCHANGED, resize
 
 
 class Image:
-    __img = None
-
     def __init__(self, path, gray=True):
         self.__path = path
         self.__gray = gray
 
-        self.__read()
+        self.__img = self.__read()
 
     def __call__(self, *args, **kwargs):
         return self.__img
 
     def __read(self):
-        self.__img = imread(
+        return imread(
             self.__path,
             IMREAD_GRAYSCALE
             if self.__gray
